1ee1fd1140ed9a6b46f45619abe359a3a608b8c1
Nick Terner <viseguy@gmail.com>
2015-08-19 17:18:52 +0300

    fix formatting

--- analyzer/index.js
+++ analyzer/index.js
@@ -10,82 +10,82 @@ var git          =   require('gift');
 var filewalker   =   require('filewalker');
 
 var runner = function (folder){
-  EventEmitter.call(this);
-  if(!fs.existsSync(folder)) console.warn('folder does not exists');
-  this.rules = [];
-  this.folder = folder;
-  this.stack = [];
-  this.supported = ['js', 'cs', 'java', 'php'];
-  this.dockerfiles = [];
-  this.gruntfiles = [];
-  this.packageJson = [];
-  this.use(metaData);
+    EventEmitter.call(this);
+    if(!fs.existsSync(folder)) console.warn('folder does not exists');
+    this.rules = [];
+    this.folder = folder;
+    this.stack = [];
+    this.supported = ['js', 'cs', 'java', 'php'];
+    this.dockerfiles = [];
+    this.gruntfiles = [];
+    this.packageJson = [];
+    this.use(metaData);
 };
 
 util.inherits(runner, EventEmitter);
 runner.prototype.cloneFromGit = function(repoUrl , targetFolder){
-  var defer = Q.defer();
-  var self = this;
+    var defer = Q.defer();
+    var self = this;
   /*if (fs.existsSync(targetFolder)){
         var p = defer.promise;
         defer.reject('folder' + targetFolder +  'already exists');
         return Q.when(p);
   }*/
 
-  git.clone(repoUrl, targetFolder, function (err, _repo){
-  console.log('repo created');
-  if (err)
-     defer.reject(err);
-   else{
-     defer.resolve(_repo);
-     self.folder = targetFolder;
-  }
-});
+    git.clone(repoUrl, targetFolder, function (err, _repo){
+        console.log('repo created');
+        if (err)
+            defer.reject(err);
+        else{
+            defer.resolve(_repo);
+            self.folder = targetFolder;
+        }
+    });
 
-  return defer.promise;
+    return defer.promise;
 };
 runner.prototype.use = function(rule)
 {
-  console.log('rule added');
-  this.rules.push(rule);
+    console.log('rule added');
+    this.rules.push(rule);
 };
 
 runner.prototype.start = function(){
-  this.context = {};
-  var self = this;
-  var defer = Q.defer();
-  this.handle = defer;
-  async.eachSeries(this.rules, function iterator(rule, callback) {
-      rule.call(self, self.folder, self.context, callback);
-}, function done(err, result) {
+    this.context = {};
+    var self = this;
+    var defer = Q.defer();
+    this.handle = defer;
+    async.eachSeries(this.rules, function iterator(rule, callback) {
+        rule.call(self, self.folder, self.context, callback);
+    }, function done(err, result) {
 
 
-    console.log('--------------------------------------');
-    (err) ? console.log('!!!err:' + err)  : "";
+        console.log('--------------------------------------');
+        (err) ? console.log('!!!err:' + err)  : "";
 
-    if (err)
-    return defer.reject(err);
+        if (err)
+            return defer.reject(err);
 
-    var ret  = [self.dockerfiles, self.stack, self.gruntfiles, self.packageJson];
+        var ret  = [self.dockerfiles, self.stack, self.gruntfiles, self.packageJson];
 
-    defer.resolve(ret);
+        defer.resolve(ret);
 
-    console.log('--------------------------------------');
-});
+        console.log('--------------------------------------');
+    });
 
- return defer.promise;
+    return defer.promise;
 };
 
 var parsePackage = function(data){
-  var t = JSON.parse(fs.readFileSync(data));
-  console.log("package.json:" + JSON.stringify(t.scripts));
+    var t = JSON.parse(fs.readFileSync(data));
+    console.log("package.json:" + JSON.stringify(t.scripts));
 
-  function delay(ms) {
-    var deferred = Q.defer();
-    setTimeout(deferred.resolve, ms);
-    return deferred.promise;
- }
- return delay(2000);
+    function delay(ms) {
+        var deferred = Q.defer();
+        setTimeout(deferred.resolve, ms);
+        return deferred.promise;
+    }
+    return delay(2000);
 };
 
 runner.prototype.done = function(callback)
@@ -105,49 +105,49 @@ function gitInfo(folder, output, callback){
 
 function metaData(folder, output ,next){
 
-  var emitter = this;
-  filewalker(folder)
+    var emitter = this;
+    filewalker(folder)
     .on('dir', function(p) {
-      //console.log('dir:  %s', p);
+        //console.log('dir:  %s', p);
     })
     .on('file', function(p, s) {
-     var data = path.parse(p);
-     emitter.handle.notify(p);
+        var data = path.parse(p);
+        emitter.handle.notify(p);
 
-    if ((data.base.toLowerCase().indexOf("dockerfile") !== -1) || (data.base.toLowerCase().indexOf("docker-compose") !== -1)){
-      console.log('dockefile was detected:' + p);
-      emitter.dockerfiles.push(p);
-      return emitter.emit('docker', data);
-    }
-     if (data.base === 'package.json'){
-         emitter.packageJson.push(p);
-        parsePackage(path.resolve(folder, p)).then(function(data){emitter.emit('packageJson', data)}, function(err){
-          emitter.emit('error:', err);
-        });
-        //emitter.emit('nodejs', p);
-      }
+        if ((data.base.toLowerCase().indexOf("dockerfile") !== -1) || (data.base.toLowerCase().indexOf("docker-compose") !== -1)){
+            console.log('dockefile was detected:' + p);
+            emitter.dockerfiles.push(p);
+            return emitter.emit('docker', data);
+        }
+        if (data.base === 'package.json'){
+            emitter.packageJson.push(p);
+            parsePackage(path.resolve(folder, p)).then(function(data){emitter.emit('packageJson', data)}, function(err){
+                emitter.emit('error:', err);
+            });
+            //emitter.emit('nodejs', p);
+        }
 
-      if (data.base.toLowerCase().indexOf("gruntfile") !== -1){
-        console.log('gruntfile was detected:' + p);
-        emitter.gruntfiles.push(p);
-        return emitter.emit('gruntfile', p);
-      }
-    //  console.log('file: %s, %d bytes', data.base, s.size);
-     var ext = data.ext.slice(- (data.ext.length - 1));
-     if (_.indexOf(emitter.stack, ext) === -1 && _.indexOf(emitter.supported, ext)!== -1){
-          console.log('stack found ' + ext);
-          emitter.stack.push(ext);
-          emitter.emit('stack', ext);
+        if (data.base.toLowerCase().indexOf("gruntfile") !== -1){
+            console.log('gruntfile was detected:' + p);
+            emitter.gruntfiles.push(p);
+            return emitter.emit('gruntfile', p);
+        }
+        //  console.log('file: %s, %d bytes', data.base, s.size);
+        var ext = data.ext.slice(- (data.ext.length - 1));
+        if (_.indexOf(emitter.stack, ext) === -1 && _.indexOf(emitter.supported, ext)!== -1){
+            console.log('stack found ' + ext);
+            emitter.stack.push(ext);
+            emitter.emit('stack', ext);
         }
 
     }).on('error', function(err) {
-      console.error(err);
+        console.error(err);
     })
     .on('done', function() {
-      emitter.emit('done', null, emitter.stack);
-      next();
+        emitter.emit('done', null, emitter.stack);
+        next();
     })
-  .walk();
+    .walk();
 }
 
 module.exports = runner;
--- gitClone/index.spec.js
+++ gitClone/index.spec.js
@@ -1,158 +1,158 @@
 
 describe ('git test', function(){
-var git     = require('gift');
-var  _      = require('lodash');
-var  Q      = require('q');
-var path    = require('path');
-var assert  = require('assert');
-var repos = [
-  "https://github.com/Codefresh-Examples/lets-chat.git",
-  "https://github.com/Codefresh-Examples/socket-io.git",
-  "https://github.com/Codefresh-Examples/jsbeautify.git",
-  "https://github.com/Codefresh-Examples/express-angular.git",
-  "https://github.com/zwij/wap_zaverecna.git",
-  "https://github.com/yaronr/dockerfile.git",
-  "https://github.com/codefresh-io/spigo.git"
-];
-var docker = 5;
-var js = 1;
-var testFolder = process.env.TEST_FOLDER || './test/repo';
+    var git     = require('gift');
+    var  _      = require('lodash');
+    var  Q      = require('q');
+    var path    = require('path');
+    var assert  = require('assert');
+    var repos = [
+        "https://github.com/Codefresh-Examples/lets-chat.git",
+        "https://github.com/Codefresh-Examples/socket-io.git",
+        "https://github.com/Codefresh-Examples/jsbeautify.git",
+        "https://github.com/Codefresh-Examples/express-angular.git",
+        "https://github.com/zwij/wap_zaverecna.git",
+        "https://github.com/yaronr/dockerfile.git",
+        "https://github.com/codefresh-io/spigo.git"
+    ];
+    var docker = 5;
+    var js = 1;
+    var testFolder = process.env.TEST_FOLDER || './test/repo';
 
-function only(index)
-{
-  repos = repos.slice(index, index + 1);
-}
-it.skip('set docker repo' , function(){
-    only(js);
-    console.log(JSON.stringify(docker));
-});
-
-it('just git clone', function(done){
-  this.timeout(20000);
-  var index = 0;
-  var promises =  _.map(repos, function(repo){
-
-      console.log('cloing repo :' + JSON.stringify(repo));
-      index++;
-      var defer = Q.defer();
-      var localDir = path.resolve(__dirname, testFolder, index.toString());
-      console.log('local folder:' + localDir);
-      git.clone(repo, localDir, function (err, _repo){
-      console.log('repo created err:' + err);
-
-      if (err) return defer.reject(err);
-        defer.resolve(_repo);
-      });
-
-      return defer.promise;
+    function only(index)
+    {
+        repos = repos.slice(index, index + 1);
+    }
+    it.skip('set docker repo' , function(){
+        only(js);
+        console.log(JSON.stringify(docker));
     });
 
-    Q.all(promises).then(function(result){
-      console.log('cloned with result : ' + JSON.stringify(result));
-      done();},
-      function(err){
-        done(err);});
-});
-it('run on spigo repo' , function(done){
-  this.timeout(10000);
+    it('just git clone', function(done){
+        this.timeout(20000);
+        var index = 0;
+        var promises =  _.map(repos, function(repo){
 
-  index = 0;
-  var promises = [];
-  var Runner = require('../analyzer');
-  assert(Runner);
+            console.log('cloing repo :' + JSON.stringify(repo));
+            index++;
+            var defer = Q.defer();
+            var localDir = path.resolve(__dirname, testFolder, index.toString());
+            console.log('local folder:' + localDir);
+            git.clone(repo, localDir, function (err, _repo){
+                console.log('repo created err:' + err);
 
-  var localDir = path.resolve(__dirname, testFolder, index.toString());
+                if (err) return defer.reject(err);
+                defer.resolve(_repo);
+            });
 
-  var r = new Runner(localDir);
-  var p = r.start();
-  r.on('packageJson', function(data){
-    console.log('package json found :'  + data);
-  });
-
-});
-
-it('using git clone [node]' , function(done){
-     console.log('git clone use case');
-
-     var repoUrl = "https://github.com/codefresh-io/codefresh-io.git";
-     var git = require('gift');
-     var localDir = path.resolve(__dirname, testFolder);
-     this.timeout(2000000);
-
-     console.log('clonning repo to ' + localDir);
-     git.clone(repoUrl, localDir, function (err, _repo){
-     console.log('repo created');
-
-      if (err)
-        done(err);
-
-      var r = new Runner(localDir);
-      r.on('docker:dockefiles', function(p){
-         console.log('docker file was detected:' + p);
-       });
-       r.start();
-       r.done(function(err, data){
-         done(err);});
-     });
- });
-
- it('using git clone php' , function(done){
-      console.log('git clone use case');
-      var Runner = require('../analyzer');
-      assert(Runner);
-
-      var repoUrl = "https://github.com/zwij/wap_zaverecna.git";
-      var git = require('gift');
-      var localDir = path.resolve(__dirname, testFolder);
-      this.timeout(2000000);
-
-      console.log('clonning repo to ' + localDir);
-      git.clone(repoUrl, localDir, function (err, _repo){
-      console.log('repo created');
-
-       if (err)
-         done(err);
-
-       var r = new Runner(localDir);
-       r.on('docker:dockefiles', function(p){
-          console.log('docker file was detected:' + p);
+            return defer.promise;
         });
-        r.start();
-        r.done(function(err, data){
-          done(err);});
-      });
-  });
 
-it('run on multiple repos' , function(done){
-  this.timeout(10000);
+        Q.all(promises).then(function(result){
+            console.log('cloned with result : ' + JSON.stringify(result));
+            done();},
+            function(err){
+                done(err);});
+    });
+    it('run on spigo repo' , function(done){
+        this.timeout(10000);
 
-  index = 0;
-  var promises = [];
-  var Runner = require('../analyzer');
-  assert(Runner);
-  _.forEach(repos, function(r){
-  index++;
-  var defer = Q.defer();
-  var localDir = path.resolve(__dirname, testFolder, index.toString());
-  done();
-  var r = new Runner(localDir);
-  var p = r.start();
-  r.on('packageJson', function(data){
-    console.log('repo name is :' + r);
-    console.log('package json found :'  + data);
-  });
-  promises.push(p);
-});
+        index = 0;
+        var promises = [];
+        var Runner = require('../analyzer');
+        assert(Runner);
 
+        var localDir = path.resolve(__dirname, testFolder, index.toString());
 
-  Q.all(promises).then(function(result){
-    _.forEach(result, function(r){
-        console.log('cloned with result : ' + JSON.stringify(r));
+        var r = new Runner(localDir);
+        var p = r.start();
+        r.on('packageJson', function(data){
+            console.log('package json found :'  + data);
+        });
+
     });
 
-    done();},
-    function(err){
-      done(err);});
+    it('using git clone [node]' , function(done){
+        console.log('git clone use case');
 
-});
+        var repoUrl = "https://github.com/codefresh-io/codefresh-io.git";
+        var git = require('gift');
+        var localDir = path.resolve(__dirname, testFolder);
+        this.timeout(2000000);
+
+        console.log('clonning repo to ' + localDir);
+        git.clone(repoUrl, localDir, function (err, _repo){
+            console.log('repo created');
+
+            if (err)
+                done(err);
+
+            var r = new Runner(localDir);
+            r.on('docker:dockefiles', function(p){
+                console.log('docker file was detected:' + p);
+            });
+            r.start();
+            r.done(function(err, data){
+                done(err);});
+        });
+    });
+
+    it('using git clone php' , function(done){
+        console.log('git clone use case');
+        var Runner = require('../analyzer');
+        assert(Runner);
+
+        var repoUrl = "https://github.com/zwij/wap_zaverecna.git";
+        var git = require('gift');
+        var localDir = path.resolve(__dirname, testFolder);
+        this.timeout(2000000);
+
+        console.log('clonning repo to ' + localDir);
+        git.clone(repoUrl, localDir, function (err, _repo){
+            console.log('repo created');
+
+            if (err)
+                done(err);
+
+            var r = new Runner(localDir);
+            r.on('docker:dockefiles', function(p){
+                console.log('docker file was detected:' + p);
+            });
+            r.start();
+            r.done(function(err, data){
+                done(err);});
+        });
+    });
+
+    it('run on multiple repos' , function(done){
+        this.timeout(10000);
+
+        index = 0;
+        var promises = [];
+        var Runner = require('../analyzer');
+        assert(Runner);
+        _.forEach(repos, function(r){
+            index++;
+            var defer = Q.defer();
+            var localDir = path.resolve(__dirname, testFolder, index.toString());
+            done();
+            var r = new Runner(localDir);
+            var p = r.start();
+            r.on('packageJson', function(data){
+                console.log('repo name is :' + r);
+                console.log('package json found :'  + data);
+            });
+            promises.push(p);
+        });
+
+
+        Q.all(promises).then(function(result){
+            _.forEach(result, function(r){
+                console.log('cloned with result : ' + JSON.stringify(r));
+            });
+
+            done();},
+            function(err){
+                done(err);});
+
+    });
 });
